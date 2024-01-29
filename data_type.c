450ad7e2b9aa91d21df627867254f2daa43f5088
loco-c <carloscustodio9@gmail.com>
2024-01-29 23:53:16 +0000

    Added basic data types

---
+++ data_type.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h> // for bool type
+
+int main() {
+
+    int age = 28;
+    float height = 1.8;
+    double weight = 100.;
+    char grade = 'A'; 
+    bool isHappy = true;
+
+
+
+}
